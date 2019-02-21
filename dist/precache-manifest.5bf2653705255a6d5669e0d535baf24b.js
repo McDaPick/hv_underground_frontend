edcaf8179070b7bc4c63270bb1bad84d8973b4e0
Mikey Nielsen <michaelnielsen260@gmail.com>
2019-02-21 00:33:01 -0500

    pushing to heroku

---
+++ dist/precache-manifest.5bf2653705255a6d5669e0d535baf24b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2a584339993350f30eef",
+    "url": "/js/chunk-vendors.d935eb33.js"
+  },
+  {
+    "revision": "787b4d9f12e3f42e1b72",
+    "url": "/js/app.88425250.js"
+  },
+  {
+    "revision": "24a599122f56ad0b5770",
+    "url": "/js/about.40982c22.js"
+  },
+  {
+    "revision": "68666aeacc1df4db31f3f0a3a2ca9791",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "787b4d9f12e3f42e1b72",
+    "url": "/css/app.ccd21fcc.css"
+  }
+];
