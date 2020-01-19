76aed98d0b0b39412b6519b99c6a5348fac83bfe
Michael Nielsen <michael.nielsen@marist.edu>
2020-01-19 18:43:53 -0500

    added google fonts

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <link href='https://fonts.googleapis.com/css?family=Graduate' rel='stylesheet'>
-    <link href="https://fonts.googleapis.com/css?family=Gelasio&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=BenchNine|News+Cycle|Poppins:700&display=swap" rel="stylesheet">
+
     <div class="container-fluid" id="photo">
       <div class="row">
         <img src = "https://images.unsplash.com/photo-1481886756534-97af88ccb438?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80" style="z-index: 1000;width: 100%;">
@@ -11,27 +11,27 @@
   <div id ="featbandinfo" class="container-fluid" style="text-align:left;">
     <div class = "row">
         <div class = "col-lg-8" id="bio" style="padding-top:15px;padding-left: 35px;background-image: linear-gradient(to top, rgba(0,0,0,1), rgba(0,0,0,0.4));color:white;">
-          <h1 style="">Dirty Mike and the Bois</h1>
-          <div class="container">
+          <h1 style="font-family: 'Poppins', sans-serif;">Dirty Mike and the Bois</h1>
+          <div class="container" style="font-family: 'BenchNine', sans-serif;">
             <div class="row">
-              <div class="col-4">
+              <div class="col-4" style="padding-left: 0px;">
                 <p>Gerne: Metal</p>
 
               </div>
-              <div class="col-4">
+              <div class="col-4" style="padding-left: 0px;">
                 <p>Location: Cornwall, NY</p>
               </div>
               <div class="col-4">
               </div>
             </div>
           </div>
-        <hr>
-            <p style="font-family:Gelasio; word-wrap: break-word;">
-              BioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBio
-              BioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBio
-              BioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBioBio
-              BioBioBioBioBioBio
-              Bio
+        <hr style="    border-top: 1px solid white;">
+            <p style="font-family: 'BenchNine', sans-serif;word-wrap: break-word;font-size:19px;">
+              This is a really cool bio. This is a really cool bio. This is a really cool bio. This is a really cool bio.
+              This is a really cool bio. This is a really cool bio. This is a really cool bio. This is a really cool bio.
+              This is a really cool bio. This is a really cool bio. This is a really cool bio.
+              This is a really cool bio. This is a really cool bio.
+              This is a really cool bio.
             </p>
         </div>
         <div class = "col-lg-4" style="background-image: linear-gradient(to top, rgba(0,0,0,1), rgba(0,0,0,0.4)); padding-right:0px; padding-left:0px;">
